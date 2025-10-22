2ac3b620ccca0d4c61877e6560873e6d57d15dc5
Han <gyungchaehan@gmail.com>
2025-10-22 13:57:30 +0900

    edit config

--- src/config.ts
+++ src/config.ts
@@ -3,10 +3,10 @@ import type { ThemeConfig } from './types'
 export const themeConfig: ThemeConfig = {
   // SITE INFO ///////////////////////////////////////////////////////////////////////////////////////////
   site: {
-    website: 'https://astro-chiri.netlify.app/', // Site domain
-    title: 'CHIRI', // Site title
-    author: '3ASH', // Author name
-    description: 'Minimal blog built by Astro', // Site description
+    website: 'https://snowhan.com/', // Site domain
+    title: 'blog', // Site title
+    author: 'Han', // Author name
+    description: 'Personal blog built with Astro & Chiri', // Site description
     language: 'en-US' // Default language
   },
 
